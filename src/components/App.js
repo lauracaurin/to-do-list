77da43b1d38cacfd67d1c49eee0c62e1bbc473e1
lauracaurin <laura_cd1@hotmail.com>
2024-08-02 11:42:00 +0100

    Publish in GitHub Pages

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../styles/App.scss';
+// Ej img: import logoMenu from '../images/ico-menu.svg';
+
+function App() {
+  return (
+    <div>
+      {/* Aquí va todo el contenido del body */}
+    </div>
+  );
+}
+export default App;
