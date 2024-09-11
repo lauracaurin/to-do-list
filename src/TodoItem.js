9e018bf69f8138b3c8bd386576595d69ca0941c4
lauracaurin <laura_cd1@hotmail.com>
2024-09-11 18:34:04 +0100

    Icons with dynamic colors

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -7,21 +7,15 @@ function TodoItem({ text, completed, onComplete, onDelete }) {
 
   return (
     <li className="TodoItem">
-      <CompleteIcon />
-      {/* <span
-        className={`Icon Icon-check ${completed && "Icon-check--active"}`}
-        onClick={onComplete}
-      >
-        V
-      </span> */}
+      <CompleteIcon
+        completed={completed}
+        onComplete={onComplete}
+      />
       <p className={`TodoItem-p ${completed && "TodoItem-p--complete"} `}>{text}</p>
 
-      <DeleteIcon />
-      {/* <span
-        className={`Icon Icon-delete`}
-        onClick={onDelete}
-      >
-        X</span> */}
+      <DeleteIcon
+        onDelete={onDelete}
+      />
     </li >
   )
 }
