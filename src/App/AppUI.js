ec87a6901c29fc8b8867caab3a026e65191ab898
lauracaurin <laura_cd1@hotmail.com>
2024-09-12 10:53:22 +0100

    Fix useLocalStorage hook by including dependencies, updating states with useEffect, and improving error handling.

---
+++ src/App/AppUI.js
@@ -0,0 +1,38 @@
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { CreateTodoButton } from '../CreateTodoButton';
+
+function AppUI({ completedTodos, totalTodos, searchValue, setSearchValue, searchedTodos, completeTodo, deleteTodo, loading, error }) {
+  return (
+    <>
+      <TodoCounter
+        completed={completedTodos}
+        total={totalTodos} />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue} />
+
+      <TodoList>
+        {loading && <p>Cargando...</p>}
+        {error && <p>Hubo un error.</p>}
+        {(!loading && searchedTodos.length === 0) && <p> Crea tu primer TODO</p>}
+
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList >
+
+      <CreateTodoButton />
+    </>
+  )
+}
+
+export { AppUI };
