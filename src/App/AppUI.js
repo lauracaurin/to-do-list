9b8a20f6cea5a1cf8f07835a71772a2cc5746489
lauracaurin <laura_cd1@hotmail.com>
2024-09-12 11:37:46 +0100

    Add loading skeletons for improved UX

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -2,6 +2,9 @@ import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
+import { TodosLoading } from '../TodosLoading';
+import { TodosError } from '../TodosError';
+import { EmptyTodos } from '../EmptyTodos';
 import { CreateTodoButton } from '../CreateTodoButton';
 
 function AppUI({ completedTodos, totalTodos, searchValue, setSearchValue, searchedTodos, completeTodo, deleteTodo, loading, error }) {
@@ -15,9 +18,15 @@ function AppUI({ completedTodos, totalTodos, searchValue, setSearchValue, search
         setSearchValue={setSearchValue} />
 
       <TodoList>
-        {loading && <p>Cargando...</p>}
-        {error && <p>Hubo un error.</p>}
-        {(!loading && searchedTodos.length === 0) && <p> Crea tu primer TODO</p>}
+        {loading && (
+          <>
+            <TodosLoading />
+            <TodosLoading />
+            <TodosLoading />
+          </>
+        )}
+        {error && <TodosError />}
+        {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
 
         {searchedTodos.map(todo => (
           <TodoItem
